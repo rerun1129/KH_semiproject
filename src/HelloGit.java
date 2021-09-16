53c1e38184948d3943e9f54f1dd2711d88a01970
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-09-16 22:11:49 +0900

    브랜치 버전5

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,7 +3,7 @@ public class HelloGit {
     public static void main(String[] args) {
 
         System.out.println("안녕하세요. 1조 여러분 환영합니다.");
-        System.out.println("저는 브랜치 1입니다.");
+        System.out.println("저는 입니다.");
         System.out.println("이 이제 제겁니다.");
         System.out.println("이 브랜치는 이제 제겁니다.");
 
