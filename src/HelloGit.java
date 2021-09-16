9f0c0ea9832827e3438f916953396ff7bee59112
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-09-16 20:51:03 +0900

    스테이징 후 커밋입니다.

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,7 +2,7 @@ public class HelloGit {
 
     public static void main(String[] args) {
 
-        System.out.println("안녕하세요 브랜치입니다.");
+        System.out.println("안녕하세요 브랜치1 입니다. 안녕들 하시죠?");
 
     }
 }
